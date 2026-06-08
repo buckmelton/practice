ac52e6b891647e9b26b97c04d9b4ea347c00f66c
Buck Melton <buck9063@gmail.com>
2026-06-07 19:45:54 -0700

    Improve seeds

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
   gem "rspec-rails", "~> 4.0.1"
   gem "factory_bot_rails"
+  gem 'faker', '~> 3.4', '>= 3.4.2'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,42 @@
 # - Each doctor should have 10 patients with unique names
 # - Each patient should have 10 appointments (5 in the past, 5 in the future)
 #   - Each appointment should be 50 minutes in duration
+
+DOCTOR_COUNT = 10
+PATIENTS_PER_DOCTOR = 10
+PAST_APPOINTMENTS_PER_PATIENT = 5
+FUTURE_APPOINTMENTS_PER_PATIENT = 5
+
+Appointment.delete_all
+Patient.delete_all
+Doctor.delete_all
+
+DOCTOR_COUNT.times do
+  doctor = Doctor.create!(
+    name: Faker::Name.name
+  )
+
+  PATIENTS_PER_DOCTOR.times do
+    patient = doctor.patients.create!(
+      name: Faker::Name.name
+    )
+
+    # past appointments
+    5.times do |k|
+      patient.appointments.create!(
+        doctor: patient.doctor,
+        duration_in_minutes: 50,
+        start_time: Time.current - k.weeks - 1
+      )
+    end
+
+    # future appointments
+    5.times do |k|
+      patient.appointments.create!(
+        doctor: patient.doctor,
+        duration_in_minutes: 50,
+        start_time: Time.current + k.weeks
+      )
+    end
+  end
+end
