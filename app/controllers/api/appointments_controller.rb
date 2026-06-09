7a463108495d40a75e5526ef7fc6f38b40df93af
Buck Melton <buck9063@gmail.com>
2026-06-09 10:25:27 -0700

    Filter AppointmentsController#index on doctor_id

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -2,16 +2,27 @@ class Api::AppointmentsController < ApplicationController
   def index
     # TODO: return all values
     @appts = Appointment.includes(:doctor, :patient)
+
+    # TODO: return filtered values
+    if index_params[:doctor_id].present?
+      @appts = @appts.where(doctor_id: index_params[:doctor_id])
+    end
+
     render json: @appts, include:
       {
         doctor: { only: [:id, :name] },
         patient: { only: [:id, :name] }
       }
-    # TODO: return filtered values
     # head :ok
   end
 
   def create
     # TODO:
+  end
+
+  private
+
+  def index_params
+    params.permit(:doctor_id)
   end
 end
--- spec/controllers/api/appointments_controller_spec.rb
+++ spec/controllers/api/appointments_controller_spec.rb
@@ -5,16 +5,34 @@ RSpec.describe Api::AppointmentsController do
     let!(:pat1) { FactoryBot.create(:patient, doctor: doc1) }
     let!(:appt1) { FactoryBot.create(:appointment, doctor: doc1, patient: pat1) }
 
+    let!(:doc2) { FactoryBot.create(:doctor) }
+    let!(:pat2) { FactoryBot.create(:patient, doctor: doc2) }
+    let!(:appt2) { FactoryBot.create(:appointment, doctor: doc2, patient: pat2) }
+
+    
     it "returns json data" do
       get :index
       expect(response.status).to eq(200)
 
       res_appts = JSON.parse(response.body)
       expect(res_appts).to be_present
-      expect(res_appts.length).to eq(1)
+      expect(res_appts.length).to eq(2)
       res_appt = res_appts.first
       expect(res_appt["doctor"]["name"]).to eq(doc1.name)
       expect(res_appt["patient"]["name"]).to eq(pat1.name)
+    end
+
+    it "filters on doctor" do
+      get :index, params: { doctor_id: doc2.id }
+
+      expect(response.status).to eq(200)
+
+      res_appts = JSON.parse(response.body)
+      expect(res_appts).to be_present
+      expect(res_appts.length).to eq(1)
+      res_appt = res_appts.first
+      expect(res_appt["doctor"]["name"]).to eq(doc2.name)
+      expect(res_appt["patient"]["name"]).to eq(pat2.name)
     end
   end
 end
