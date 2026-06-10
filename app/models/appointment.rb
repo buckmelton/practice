d582329b96418e77a9c256216ecceb2ee00b1485
Buck Melton <buck9063@gmail.com>
2026-06-10 10:56:28 -0700

    Prevent overlapping appointments for a doctor

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,12 +6,29 @@ class Appointment < ApplicationRecord
   validates :duration_in_minutes, presence: true, numericality: { only: :integer }
 
   validate :patient_belongs_to_doctor
+  validate :doctor_is_available
 
   private
 
   def patient_belongs_to_doctor
     return if patient.blank? || doctor.blank?
-
     errors.add(:patient, "does not belong to doctor") if patient.doctor_id != doctor.id
+  end
+
+  def new_end_time
+    start_time + duration_in_minutes.minutes
+  end
+
+  def doctor_is_available
+    overlapping = Appointment
+      .where(doctor_id: doctor_id)
+      .where.not(id: id)
+      .where(
+        "start_time < ? AND start_time + (duration_in_minutes * interval '1 minute') > ?",
+        new_end_time,
+        start_time
+      )
+
+    errors.add(:start_time, "conflicts with an existing appointment for this doctor") if overlapping.exists?
   end
 end
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -3,4 +3,14 @@ RSpec.describe Appointment do
   it "creates an appointment" do
     expect { FactoryBot.create(:appointment) }.to change(Appointment, :count).by(1)
   end
+
+  let!(:doc1) { FactoryBot.create(:doctor) }
+  let!(:pat1) { FactoryBot.create(:patient, doctor: doc1) }
+  let!(:appt1) { FactoryBot.create(:appointment, doctor: doc1, patient: pat1, start_time: Time.current, duration_in_minutes: 50) }
+
+  it "prevents overlapping appointments" do
+    appt = Appointment.new(doctor_id: doc1.id, patient_id: pat1.id, start_time: Time.current, duration_in_minutes: 50 )
+    
+    expect(appt).not_to be_valid
+  end
 end
