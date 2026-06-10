6f52a0d3a29e1b91e494857c6c6d652d646c9a52
Buck Melton <buck9063@gmail.com>
2026-06-09 17:53:20 -0700

    Add validations to Appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,17 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
+
+  validates :start_time, presence: true
+  validates :duration_in_minutes, presence: true, numericality: { only: :integer }
+
+  validate :patient_belongs_to_doctor
+
+  private
+
+  def patient_belongs_to_doctor
+    return if patient.blank? || doctor.blank?
+
+    errors.add(:patient, "does not belong to doctor") if patient.doctor_id != doctor.id
+  end
 end
--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :appointment do
     doctor { create(:doctor) }
-    patient { create(:patient) }
+    patient { create(:patient, doctor: doctor) }
     start_time { Time.zone.now }
     duration_in_minutes { 50 }
   end
